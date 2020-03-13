a0c1454dc2b87efe5e0e1f28aeb20aadc3d5f5e3
Jonas Gustafson <jonas.gustafson@skandnet.com>
2020-03-13 09:51:29 +0100

    Updates

---
+++ precache-manifest.3d5d78f2a56a50e699a144f66f443b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c103750f8aca5b68677cd3bbbd0119c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907952c03a412311fd43",
+    "url": "/static/css/main.ebe9d9e1.chunk.css"
+  },
+  {
+    "revision": "9215ccec326c6b83c0e9",
+    "url": "/static/js/2.3ac8d5f4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3ac8d5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907952c03a412311fd43",
+    "url": "/static/js/main.60dd0da9.chunk.js"
+  },
+  {
+    "revision": "a971d6f960ffaa32ea84",
+    "url": "/static/js/runtime-main.46b74744.js"
+  },
+  {
+    "revision": "81d5ab3bbd799f7c3d721712a2aeebdc",
+    "url": "/static/media/sort-icon.81d5ab3b.svg"
+  }
+]);
