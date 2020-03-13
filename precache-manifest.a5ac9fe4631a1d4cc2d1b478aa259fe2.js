fc566d4af9b3a83d5f823217045fa73570b6b350
Jonas Gustafson <jonas.gustafson@skandnet.com>
2020-03-13 09:09:57 +0100

    Updates

---
+++ precache-manifest.a5ac9fe4631a1d4cc2d1b478aa259fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3763126738dc019e5d5d0d5c9f8b5051",
+    "url": "/allt-om-oponion/index.html"
+  },
+  {
+    "revision": "1780ff7231d0f863cc29",
+    "url": "/allt-om-oponion/static/css/main.ebe9d9e1.chunk.css"
+  },
+  {
+    "revision": "9215ccec326c6b83c0e9",
+    "url": "/allt-om-oponion/static/js/2.3ac8d5f4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/allt-om-oponion/static/js/2.3ac8d5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1780ff7231d0f863cc29",
+    "url": "/allt-om-oponion/static/js/main.9d8e568f.chunk.js"
+  },
+  {
+    "revision": "c50872f7106ce93d3ce6",
+    "url": "/allt-om-oponion/static/js/runtime-main.5cb8ecac.js"
+  },
+  {
+    "revision": "81d5ab3bbd799f7c3d721712a2aeebdc",
+    "url": "/allt-om-oponion/static/media/sort-icon.81d5ab3b.svg"
+  }
+]);
